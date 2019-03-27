51ea095385112e2942f06e8d2079603fb5eb0848
alcavalieri85 <alfonsocavalieri@gmail.com>
2019-03-27 13:21:34 +0000

    add new React Project

---
+++ React/ravenous/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,43 @@
+// eslint-disable-next-line
+import React from 'react';
+import './SearchBar.css';
+
+const sortByOptions = {
+    'Best Match': 'best_match',
+    'Highest Rated': 'rating',
+    'Most Reviewed': 'review_count'
+};
+
+class SearchBar extends React.Component{
+    renderSortByOptions() {
+        //return the kyes of the object
+        return Object.keys(sortByOptions).map(sortByOption => {
+            //store in a variable the value of the object
+            let sortByOptionValue = sortByOptions[sortByOption];
+            return (
+                <li key={sortByOptionValue}>{sortByOption}</li>
+            );
+        });
+    }
+    
+    render(){
+        return(
+            <div className="SearchBar">
+                <div className="SearchBar-sort-options">
+                    <ul>
+                        {this.renderSortByOptions()}
+                    </ul>
+                </div>
+                <div className="SearchBar-fields">
+                    <input placeholder="Search Businesses" />
+                    <input placeholder="Where?" />
+                </div>
+                <div className="SearchBar-submit">
+                    <a>Let's Go</a>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
