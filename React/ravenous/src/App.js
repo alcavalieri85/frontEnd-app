f5acfb65d16747f2dfcca81e3685d79bb650aca6
alcavalieri85 <alfonsocavalieri@gmail.com>
2019-03-27 18:21:22 +0000

    searchbar can take value and log it with event Handler

--- React/ravenous/src/App.js
+++ React/ravenous/src/App.js
@@ -18,11 +18,15 @@ const business = {
 const businesses = [business, business, business, business, business, business];
 
 class App extends Component {
+  searchYelp(term, location, sortBy){
+      console.log("Serching a "+term+", in "+location+" sorted by "+sortBy);
+  }
+
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
-        <SearchBar />
+        <SearchBar searchYelp={this.searchYelp}/>
         <BusinessList businesses={businesses}/>
       </div>
     );
